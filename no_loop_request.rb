1d45c994dcdd9e6e69992cf6e7800669e4f76a56
Goncalo Pereira <goncalo.pereira@7digital.com>
2011-11-04 12:10:23 +0000

    loop request with and without threads for conc test

---
+++ no_loop_request.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'uri'
+
+
+def thread i
+	uri = URI(ARGV[1])
+	http = Net::HTTP.new(uri.host, uri.port)
+	path = uri.path.empty? ? "/" : uri.path
+	query = uri.query.nil? ?  "" : "?" + uri.query	
+	res = http.get(path + query, nil)
+
+	puts res.code + " " + i.to_s
+	if (res.code != '200') 
+		puts res.body
+	end
+end
+
+i = 0
+while i<ARGV[0].to_i
+ thread i
+
+i+=1
+end
+
+
+sleep
